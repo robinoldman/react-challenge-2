99ef641061c9db435a681dc811f748cafef035b6
robinoldman <robin.c.oldman@gmail.com>
2023-07-18 08:47:16 +0000

    add method for comonent did mount

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import { savedPosts } from '../posts.json';
-import css from './css/Content.module.css';
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
-    // eslint-disable-next-line
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: false
+        }
+    }
 
+    componentDidMount() {
+      setTimeout(()=>{
+          this.setState({
+              isLoaded: true,
+          })
+      }, 2000)
   }
+    
+  
 
   render() {
     return (
       <div className=  {css.Content}>
         <div className={css.TitleBar}>
           <h1>My Posts</h1>
+       
         </div>
         <div className={css.SearchResults}>
-          {savedPosts.map((post, index) => (
-            <div key={index} className={css.searchItem}>
-              <h2>{post.title}</h2>
-              <p>{post.content}</p>
-              <img src={post.image} alt="Post" />
-            </div>
-          ))}
+        {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
+
         </div>
       </div>
     );
